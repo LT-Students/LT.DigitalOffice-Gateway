c060e1bf362b4c03531a25cd9d83a1aa831faa0b
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2022-11-23 14:21:19 +0300

    fixes

--- AdminService/AdminService.Models.Dto/Models/GuiInfo.cs
+++ AdminService/AdminService.Models.Dto/Models/GuiInfo.cs
@@ -4,7 +4,7 @@ namespace LT.DigitalOffice.AdminService.Models.Dto.Models
 {
   public class GuiInfo
   {
-    public Guid? Id { get; set; }
+    public Guid Id { get; set; }
     public string PortalName { get; set; }
     public string SiteUrl { get; set; }
     public ImageConsist Logo { get; set; }
--- AdminService/AdminService.Models.Dto/Requests/CreateGuiRequest.cs
+++ AdminService/AdminService.Models.Dto/Requests/CreateGuiRequest.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using LT.DigitalOffice.AdminService.Models.Dto.Models;
+﻿using LT.DigitalOffice.AdminService.Models.Dto.Models;
 
 namespace LT.DigitalOffice.AdminService.Models.Dto.Requests
 {
   public class CreateGuiRequest
   {
-    public Guid? Id { get; set; }
     public string PortalName { get; set; }
     public string SiteUrl { get; set; }
     public ImageConsist Logo { get; set; }
